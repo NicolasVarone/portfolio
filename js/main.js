66762af2ead1924c761c2f499d11dabab79c8caa
nicolas varone <varonenico@gmail.com>
2021-11-05 17:58:44 +0100

    Add colors cursor and change animation hero title

--- js/main.js
+++ js/main.js
@@ -11,11 +11,26 @@ const cursor = (e) => {
 
 const activeCursor = (e) => {
   const item = e.target
-  if (item.classList.contains('name') || item.classList.contains('nav-item')) {
-    mouse.classList.add('active')
+  if (item.classList.contains('name')) {
+    mouse.classList.add('active', 'red-color')
+    mouseText.innerText = 'CLICK'
+  } else if (item.classList.contains('nav-item-red')) {
+    mouse.classList.add('active', 'purple-color')
+    mouseText.innerText = 'CLICK'
+  } else if (item.classList.contains('nav-item-orange')) {
+    mouse.classList.add('active', 'orange-color')
+    mouseText.innerText = 'CLICK'
+  } else if (item.classList.contains('nav-item-green')) {
+    mouse.classList.add('active', 'green-color')
     mouseText.innerText = 'CLICK'
   } else {
-    mouse.classList.remove('active')
+    mouse.classList.remove(
+      'active',
+      'purple-color',
+      'red-color',
+      'orange-color',
+      'green-color'
+    )
     mouseText.innerText = ''
   }
 }
@@ -24,16 +39,25 @@ window.addEventListener('mousemove', cursor)
 window.addEventListener('mouseover', activeCursor)
 
 /* HERO TITLE ANIMATION */
+gsap.from('.header', {
+  y: -100,
+  duration: 1,
+  delay: 0.8,
+  opacity: 0,
+  ease: 'power3.inOut',
+})
+
 gsap.from('.line1', {
-  duration: 2,
-  x: -1500,
-  delay: 0.2,
-  ease: 'Back.easeOut',
+  y: 100,
+  duration: 1,
+  opacity: 0,
+  ease: 'power3.inOut',
 })
 
 gsap.from('.line2', {
-  duration: 2,
-  x: 1500,
-  delay: 0.2,
-  ease: 'Back.easeOut',
+  y: 100,
+  duration: 1,
+  delay: 0.4,
+  opacity: 0,
+  ease: 'power3.inOut',
 })
